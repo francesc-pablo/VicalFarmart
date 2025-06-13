544cf0105edd8f19a3572db2b87f4e7900f3a2fd
William Amponsah <Francescpacopablo@gmail.com>
2025-06-13 17:37:22 +0000

    make the icons a little bit bigger

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -15,7 +15,7 @@ import {
 
 // Colorful SVG Icon Components
 const FruitIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-3 shrink-0">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mr-3 shrink-0">
     <path d="M16.5966 6.55163C17.5604 7.64448 18.1095 9.20892 18.0069 10.963C17.8435 13.7335 15.8642 16.0371 13.3853 16.436C10.9063 16.8349 8.57501 15.2289 7.61122 12.9254C6.64743 10.6218 7.33703 7.98976 9.17042 6.40243C10.6629 5.12061 12.875 4.88856 14.536 5.56845C15.2894 5.88972 15.9085 6.01256 16.5966 6.55163Z" fill="#F87171"/> {/* Apple Body - Red */}
     <path d="M14.2492 6.1368C14.2492 6.1368 15.2863 4.29369 16.6669 4.96035C18.0475 5.62702 16.7909 7.47013 16.7909 7.47013L14.2492 6.1368Z" fill="#4ADE80"/> {/* Leaf - Green */}
     <path d="M12.5 2C12.5 2 12 2.5 12 3V5C12 5.5 12.5 6 13 6C13.5 6 14 5.5 14 5V3C14 2.5 13.5 2 13 2L12.5 2Z" fill="#A16207"/> {/* Stem - Brown */}
@@ -23,7 +23,7 @@ const FruitIcon = () => (
 );
 
 const VegetableIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-3 shrink-0">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mr-3 shrink-0">
     <path d="M17.25 14.75C17.25 18.0637 14.8137 20.5 11.5 20.5C8.18629 20.5 5.75 18.0637 5.75 14.75C5.75 11.4363 8.18629 9 11.5 9C14.8137 9 17.25 11.4363 17.25 14.75Z" fill="#FB923C"/> {/* Carrot Body - Orange */}
     <path d="M11.5 9L12.5 3H10.5L11.5 9Z" fill="#22C55E"/> {/* Carrot Top Stem - Darker Green */}
     <path d="M10 5L8 3.5H9.5L10 5Z" fill="#4ADE80"/> {/* Left Leaf - Lighter Green */}
@@ -32,7 +32,7 @@ const VegetableIcon = () => (
 );
 
 const GrainIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-3 shrink-0">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mr-3 shrink-0">
     <path d="M10 2L9 6.5C9 6.5 6 7.5 6 10.5C6 13.5 9.5 14 9.5 17L9 22" stroke="#FACC15" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/> {/* Main Wheat Stalk - Yellow */}
     <path d="M14 2L15 6.5C15 6.5 18 7.5 18 10.5C18 13.5 14.5 14 14.5 17L15 22" stroke="#FACC15" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/> {/* Second Wheat Stalk */}
     <path d="M6.5 8.5L9 7L10.5 8" stroke="#EAB308" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/> {/* Wheat Grains - Darker Yellow/Gold */}
@@ -43,7 +43,7 @@ const GrainIcon = () => (
 );
 
 const DairyIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-3 shrink-0">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mr-3 shrink-0">
     <rect x="6" y="9" width="12" height="11" rx="1" fill="#EFF6FF"/> {/* Milk Carton Body - Very Light Blue/White */}
     <rect x="6" y="5" width="12" height="4" rx="1" fill="#60A5FA"/> {/* Carton Top - Blue */}
     <path d="M9 12H15V14H9V12Z" fill="#93C5FD"/> {/* Label Area - Lighter Blue */}
