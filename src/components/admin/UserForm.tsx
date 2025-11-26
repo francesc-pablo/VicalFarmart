43950a23ff5841ff28389e9b2c7f00090f3f24d2
William Amponsah <Francescpacopablo@gmail.com>
2025-11-26 04:26:34 +0000

    make the input fileds that you have hidden under courier types available

--- src/components/admin/UserForm.tsx
+++ src/components/admin/UserForm.tsx
@@ -144,15 +144,6 @@ export function UserForm({ user, onSubmit, onCancel }: UserFormProps) {
     }
   }, [watchedRegion, form, user?.region]);
   
-  const courierRoleActive = watchedRole === 'courier';
-  const courierBusinessType = form.watch('businessType' as any); // Using `any` for conditional field
-  
-  useEffect(() => {
-    if (courierRoleActive && courierBusinessType && courierBusinessType !== 'sole proprietorship') {
-       // logic for other business types
-    }
-  }, [courierRoleActive, courierBusinessType]);
-
 
   useEffect(() => {
     if ((watchedRole === 'seller' || watchedRole === 'courier') && watchedBusinessRegion && watchedBusinessRegion !== NO_REGION_VALUE) {
@@ -451,22 +442,20 @@ export function UserForm({ user, onSubmit, onCancel }: UserFormProps) {
                     </FormItem>
                 )}/>
 
-                {courierBusinessType !== 'sole proprietorship' && (
-                    <>
-                        <FormField control={form.control} name="businessName" render={({ field }) => (
-                            <FormItem><FormLabel>Business Name</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>
-                        )}/>
-                        <FormField control={form.control} name="businessRegistrationNumber" render={({ field }) => (
-                           <FormItem><FormLabel>Business Registration No.</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>
-                        )}/>
-                        <FormField control={form.control} name="tinNumber" render={({ field }) => (
-                            <FormItem><FormLabel>Tax Identification Number (TIN)</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>
-                        )}/>
-                        <FormField control={form.control} name="tradeLicenseFile" render={({ field }) => (
-                            <FormItem><FormLabel>Trade License</FormLabel><FormControl><Input type="file" className="h-auto p-2" onChange={(e) => field.onChange(e.target.files ? e.target.files[0] : null)} /></FormControl>{user?.tradeLicenseUrl && (<FormDescription>Current file: <a href={user.tradeLicenseUrl} target="_blank" rel="noopener noreferrer" className="text-primary underline">View</a></FormDescription>)}<FormMessage /></FormItem>
-                        )}/>
-                    </>
-                )}
+                <>
+                    <FormField control={form.control} name="businessName" render={({ field }) => (
+                        <FormItem><FormLabel>Business Name</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>
+                    )}/>
+                    <FormField control={form.control} name="businessRegistrationNumber" render={({ field }) => (
+                        <FormItem><FormLabel>Business Registration No.</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>
+                    )}/>
+                    <FormField control={form.control} name="tinNumber" render={({ field }) => (
+                        <FormItem><FormLabel>Tax Identification Number (TIN)</FormLabel><FormControl><Input {...field} /></FormControl><FormMessage /></FormItem>
+                    )}/>
+                    <FormField control={form.control} name="tradeLicenseFile" render={({ field }) => (
+                        <FormItem><FormLabel>Trade License</FormLabel><FormControl><Input type="file" className="h-auto p-2" onChange={(e) => field.onChange(e.target.files ? e.target.files[0] : null)} /></FormControl>{user?.tradeLicenseUrl && (<FormDescription>Current file: <a href={user.tradeLicenseUrl} target="_blank" rel="noopener noreferrer" className="text-primary underline">View</a></FormDescription>)}<FormMessage /></FormItem>
+                    )}/>
+                </>
 
                 <Separator className="my-4" />
                 <h4 className="text-md font-medium">Personal & Vehicle Details</h4>
