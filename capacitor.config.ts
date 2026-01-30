5daeae01e2b54f43a702483e5f53d25371b6a05c
William Amponsah <Francescpacopablo@gmail.com>
2026-01-30 02:43:48 +0000

    So now i want to run it from my pc so change the capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.vicalfarmart.app',
   appName: 'Vical Farmart',
-  webDir: 'out',
+  webDir: '.next',
+  server: {
+    url: 'http://localhost:9002',
+    cleartext: true,
+  },
 };
 
 export default config;
