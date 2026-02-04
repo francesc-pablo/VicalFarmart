1732b6f4ccecd93a57526c15105dd9d2a1dbb767
William Amponsah <Francescpacopablo@gmail.com>
2026-02-04 02:03:53 +0000

    Still the flutterwave checkout page still fails to load the page on the

--- android/app/src/main/java/com/vicalfarmart/app/MainActivity.java
+++ android/app/src/main/java/com/vicalfarmart/app/MainActivity.java
@@ -1,14 +1,5 @@
 package com.vicalfarmart.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // This makes the webview background transparent
-    // so the camera preview can be seen behind it.
-    this.bridge.getWebView().setBackgroundColor(0x00000000);
-  }
-}
+public class MainActivity extends BridgeActivity {}
