75a54eeb617fba42152a075358a38d56373206eb
William Amponsah <Francescpacopablo@gmail.com>
2026-02-08 00:37:35 +0000

    What should my capacitor.config.ts look like for apk build?

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.vicalfarmart.app',
   appName: 'Vical Farmart',
-  webDir: '.next',
-  server: {
-    url: 'http://localhost:9002',
-    cleartext: true,
-  },
+  webDir: 'out',
 };
 
 export default config;
