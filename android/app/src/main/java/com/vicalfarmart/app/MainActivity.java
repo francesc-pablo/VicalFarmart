75cf5bb96d035c156a484178294955e0ee435113
William Amponsah <Francescpacopablo@gmail.com>
2026-02-22 04:20:04 +0000

    com

--- android/app/src/main/java/com/vicalfarmart/app/MainActivity.java
+++ android/app/src/main/java/com/vicalfarmart/app/MainActivity.java
@@ -1,10 +1,5 @@
 package com.vicalfarmart.app;
 
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-add(GoogleAuth.class);
-}});
-}
+public class MainActivity extends BridgeActivity {}
