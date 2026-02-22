9e5c8bb0eeb1a4f95e050cd97e6c3456c9785f0d
William Amponsah <Francescpacopablo@gmail.com>
2026-02-22 00:53:36 +0000

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,13 +5,13 @@ const config: CapacitorConfig = {
   appName: 'Vical Farmart',
   webDir: '.next',
   server: {
-    url: 'https://your-live-nextjs-app.com',
+    url: 'https://vicalfarmart.com',
     cleartext: true,
   },
   plugins: {
     GoogleAuth: {
       scopes: ['profile', 'email'],
-      serverClientId: 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+      serverClientId: '318375487368-r1tu57apceaum6q2sevc8rf81jl5rjsm.apps.googleusercontent.com',
       forceCodeForRefreshToken: true,
     },
   },
