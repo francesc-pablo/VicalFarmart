8f5e9eb0afcf08df4f9896db2c2854382864a64a
William Amponsah <Francescpacopablo@gmail.com>
2026-03-12 14:13:46 +0000

    C:\Users\PABLO\Desktop\Parent\Vical-Farmat\VicalFarmart\android\app\src\

--- android/app/src/main/java/com/vicalfarmart/app/MainActivity.java
+++ android/app/src/main/java/com/vicalfarmart/app/MainActivity.java
@@ -1,17 +1,5 @@
 package com.vicalfarmart.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import io.capawesome.capacitor.firebase.auth.FirebaseAuthenticationPlugin;
-import com.getcapacitor.community.barcodescanner.BarcodeScannerPlugin;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Registering plugins manually to resolve "not implemented" errors
-        registerPlugin(FirebaseAuthenticationPlugin.class);
-        registerPlugin(BarcodeScannerPlugin.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
