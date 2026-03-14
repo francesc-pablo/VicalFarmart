4656fd22b6b0622d989fc3d1db2a5e64b60381a7
William Amponsah <Francescpacopablo@gmail.com>
2026-03-14 02:09:16 +0000

    > Task :app:processDebugMainManifest FAILED Execution failed for task ':

--- android/app/src/main/java/com/vicalfarmart/app/MainActivity.java
+++ android/app/src/main/java/com/vicalfarmart/app/MainActivity.java
@@ -1,4 +1,3 @@
-
 package com.vicalfarmart.app;
 
 import android.os.Bundle;
@@ -8,6 +7,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Handle the splash screen transition.
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
     }
